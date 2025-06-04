7c6b838f82a3cf5a86773d50d6e7ef120a0a0a94
SimeonRolev <simorolev@gmail.com>
2025-06-04 22:59:41 +0300

    Right arrow - flipped

--- src/components/Crossroads.jsx
+++ src/components/Crossroads.jsx
@@ -1,65 +1,69 @@
 import React from 'react';
 import './Crossroads.css';
-import { Position } from '../models/index.js';
+import { Position, Turn } from '../models/index.js';
 
 const Crossroads = ({ task }) => {
-  const myCar = task.getMyCar();
-  const otherCars = task.getOtherCars();
-  
-  const renderCar = (car) => {
-    if (!car) return null;
-    
+    const myCar = task.getMyCar();
+    const otherCars = task.getOtherCars();
+
+    const renderCar = (car) => {
+        if (!car) return null;
+
+        return (
+            <>
+                <div className='car' style={{ backgroundColor: car.color }} />
+                {car.turn !== Turn.STRAIGHT && (
+                    <div
+                        className='arrow arrow-left'
+                        style={{ transform: car.turn === Turn.RIGHT ? 'scaleX(-1)' : '' }}
+                    />
+                )}
+            </>
+        );
+    };
+
+    // Get cars by position
+    const getCarByPosition = (position) => {
+        return otherCars.find(car => car.position === position);
+    };
+
+    const leftCar = getCarByPosition('left');
+    const rightCar = getCarByPosition('right');
+    const frontCar = getCarByPosition('front');
+
     return (
-      <div 
-        className={`car`}
-        style={{ backgroundColor: car.color }}
-      >
-        <div className="car-body"></div>
-      </div>
+        <div className="crossroads-container">
+            <div className="crossroads">
+                <div className='road-container road-bottom'>
+                    <div className='road'>
+                        {renderCar(myCar)}
+                    </div>
+                </div>
+                {/*                 <div className='road-container'>
+                    <div className='road'>
+                        {renderCar(frontCar)}
+                    </div>
+                </div>
+                <div className='road-container road-right'>
+                    <div className='road'>
+                        {renderCar(rightCar)}
+                    </div>
+                </div>
+                
+                <div className='road-container road-left'>
+                    <div className='road'>
+                        {renderCar(leftCar)}
+                    </div>
+                </div> */}
+            </div>
+
+            {/* Task description */}
+            <div className="task-info">
+                <h3>Current Task</h3>
+                <pre>{task.getDescription()}</pre>
+            </div>
+        </div>
     );
-  };
-
-  // Get cars by position
-  const getCarByPosition = (position) => {
-    return otherCars.find(car => car.position === position);
-  };
-
-  const leftCar = getCarByPosition('left');
-  const rightCar = getCarByPosition('right');
-  const frontCar = getCarByPosition('front');
-
-  return (
-    <div className="crossroads-container">
-      <div className="crossroads">
-        <div className='road-container'>
-            <div className='road'>
-                {renderCar(frontCar)}
-            </div>
-        </div>
-        <div className='road-container road-right'>
-            <div className='road'>
-                {renderCar(rightCar)}
-            </div>
-        </div>
-        <div className='road-container road-bottom'>
-            <div className='road'>
-                {renderCar(myCar)}
-            </div>
-        </div>
-        <div className='road-container road-left'>
-            <div className='road'>
-                {renderCar(leftCar)}
-            </div>
-        </div>
-      </div>
-      
-      {/* Task description */}
-      <div className="task-info">
-        <h3>Current Task</h3>
-        <pre>{task.getDescription()}</pre>
-      </div>
-    </div>
-  );
 };
 
 export default Crossroads;
